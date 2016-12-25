f33a9911be2877833e66da238dbb1b92c9f1c7d1
Nouman Anjum <nouman92@live.com>
2016-12-26 00:29:18 +0500

    - magento etup 1.9.3

--- dev/tests/functional/lib/Magento/Mtf/Util/Protocol/CurlTransport/FrontendDecorator.php
+++ dev/tests/functional/lib/Magento/Mtf/Util/Protocol/CurlTransport/FrontendDecorator.php
@@ -1,12 +1,32 @@
 <?php
 /**
- * Copyright © 2016 Magento. All rights reserved.
- * See COPYING.txt for license details.
+ * Magento
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@magento.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade Magento to newer
+ * versions in the future. If you wish to customize Magento for your
+ * needs please refer to http://www.magento.com for more information.
+ *
+ * @category    Tests
+ * @package     Tests_Functional
+ * @copyright  Copyright (c) 2006-2016 X.commerce, Inc. and affiliates (http://www.magento.com)
+ * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 
 namespace Magento\Mtf\Util\Protocol\CurlTransport;
 
-use Magento\Customer\Test\Fixture\Customer;
+use Mage\Customer\Test\Fixture\Customer;
 use Magento\Mtf\Util\Protocol\CurlInterface;
 use Magento\Mtf\Util\Protocol\CurlTransport;
 
@@ -110,15 +130,17 @@ class FrontendDecorator implements CurlInterface
      * Send request to the remote server.
      *
      * @param string $url
-     * @param mixed $params
+     * @param array $params
      * @param string $method
-     * @param mixed $headers
-     * @return void
+     * @param array $headers
+     * @throws \Exception
      */
     public function write($url, $params = [], $method = CurlInterface::POST, $headers = [])
     {
-        $headers = ['Set-Cookie:' . $this->cookies];
-        $this->transport->write($url, http_build_query($params), $method, $headers);
+        if ($this->formKey) {
+            $params['form_key'] = $this->formKey;
+        }
+        $this->transport->write($url, http_build_query($params), $method, ['Set-Cookie:' . $this->cookies]);
     }
 
     /**
